c796da23e88d252c01423ff7ce2978b9367571cc
Brian <bbinder912@gmail.com>
2018-02-14 15:43:06 -0500

    Create user and admin interfaces

--- exercises/accounts/src/account-manager.ts
+++ exercises/accounts/src/account-manager.ts
@@ -1,5 +1,15 @@
+export interface IUser {
+  email: string;
+  password: string;
+  isActive?: boolean;
+}
+
+export interface IAdmin extends IUser {
+  adminSince: Date;
+}
+
 export class AccountManager {
-  users = new Array();
+  users: IUser[] = new Array();
 
   /**
    * Create a new user account
@@ -8,10 +18,10 @@ export class AccountManager {
    * @return the new user account. An admin must activate it using activateNewUser
    * @see this.activateNewUser
    */
-  register(email, password) {
+  register(email: string, password: string): IUser {
     if(!email) throw 'Must provide an email';
     if(!password) throw 'Must provide a password';
-    let user = { email, password };
+    let user: IUser = { email, password };
     this.users.push(user);
     return user;
   }
@@ -22,7 +32,7 @@ export class AccountManager {
    * @param userToApprove Newly-registered user, who is to be activated
    * @return the updated user object, now activated
    */
-  activateNewUser(approver, userToApprove) {
+  activateNewUser(approver: IAdmin, userToApprove: IUser): IUser {
     if (!approver.adminSince) throw "Approver is not an admin!";
     userToApprove.isActive = true;
     return userToApprove;
@@ -34,10 +44,12 @@ export class AccountManager {
    * @param user an active user who you're making an admin
    * @return the updated user object, now can also be regarded as an admin
    */
-  promoteToAdmin(existingAdmin, user) {
+  promoteToAdmin(existingAdmin: IAdmin, user: IUser): IAdmin {
+    let newAdmin: IAdmin;
     if (!existingAdmin.adminSince) throw "Not an admin!";
     if (user.isActive !== true) throw "User must be active in order to be promoted to admin!";
-    user.adminSince = new Date();
-    return user;
+    newAdmin = user as IAdmin;
+    newAdmin.adminSince = new Date();
+    return newAdmin;
   }
 }
