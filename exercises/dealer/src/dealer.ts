d2c95cbc69ee08bab64cb749c8d049bc5559a352
Brian <bbinder912@gmail.com>
2018-02-14 18:19:44 -0500

    Complete card dealer exercise

--- exercises/dealer/src/dealer.ts
+++ exercises/dealer/src/dealer.ts
@@ -12,6 +12,49 @@ function shuffleArray(a: any[]) {
   }
 }
 
-// export class Dealer {
+export enum Suit {
+  Clubs, Diamonds, Hearts, Spades
+};
 
-// }
+export enum CardNumber {
+  Ace, Two, Three, Four, Five,
+  Six, Seven, Eight, Nine, Ten,
+  Jack, Queen, King
+};
+
+type Card = [Suit, CardNumber];
+
+function createDeck(): Card[] {
+  let deck: Card[] = [];
+  for (let suit = 0; suit < Object.keys(Suit).length / 2; suit++) {
+    for (let cardNumber = 0; cardNumber < Object.keys(CardNumber).length / 2; cardNumber++) {
+      deck.push([suit, cardNumber]);
+    }
+  }
+  return deck;
+}
+
+export class Dealer {
+  deck: Card[] = [];
+  constructor() {
+    this.deck = createDeck();
+    shuffleArray(this.deck)
+  }
+  //deals 5 cards
+  dealHand(numCards: number): Card[] {
+    if (numCards > this.getLength() || numCards < 0) throw 'There are not enough cards left to deal this hand';
+    return this.deck.splice(this.getLength() - numCards, numCards);
+  }
+  //returns number of cards left in deck
+  getLength(): number {
+    return this.deck.length;
+  }
+  //converts a card tuple into a string [0, 2] ==> 'Three of Clubs'
+  readCard(card: Card): string {
+    return `${CardNumber[card[1]]} of ${Suit[card[0]]}`;
+  }
+}
+
+// let myDealer = new Dealer();
+// console.log(myDealer.getLength());
+// console.log(myDealer.dealHand(52));
